a99cbeaa7a855161ee539f7c7116be5db2f48eda
Rado Kmit <radko.kmit@student.tuke.sk>
2025-11-09 20:24:44 +0100

    added ui.h/.c added few funcs

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void print_buffer(char *buffer){
+    char *ptr = buffer;
+    while(*ptr != '\0')
+        printf("%c", *ptr++);
+}
+
+void print_npc(char *name, char *msg){
+    print_buffer(name);
+    printf(": ");
+    print_buffer(msg);
+    printf("\n");
+}
+
 void main() {
-    printf("wohou!\n");
+    print_npc("Samo", "JeS!");
 }
---
+++ ui.h
@@ -0,0 +1,7 @@
+#ifdef UI_H
+
+#define UI_H
+
+
+
+#endif
